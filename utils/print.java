eb9569d56ac1567f221f331602233e8070c21c0a
Flossy-elephant <samyakahire2006@gmail.com>
2025-10-23 11:55:40 +0530

    Add files via upload

---
+++ utils/print.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class print {
+   public static void ln(Object obj){
+    System.out.println(obj);
+   }
+
+   public static void ln(){
+    System.out.println();
+   }
+
+   public static void p(Object obj){
+    System.out.print(obj);
+   }
+}
